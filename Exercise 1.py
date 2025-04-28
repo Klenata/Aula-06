7fce5cb3bfb9369720c7288e0ae85d41842ec383
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-04-28 10:37:51 -0300

    modifiquei para conferir um nome dentro da lista

--- Exercise 1.py
+++ Exercise 1.py
@@ -2,5 +2,10 @@ nomes = ["", "", "", "", ""]
 for i in range(len(nomes)):
     nomes[i] = input(f"Digite o {i+1}° nome: ")
 
+pesquisa = input("Digite o nome que você quer pesquisar: ")
 for x in range(len(nomes)):
-    print(f"{x} {nomes[x]}")
+    if pesquisa == nomes[x]:
+        print(f"Está na lista, posição {x}")
+        break
+    else:
+        print("não", end=" ")
