01b85cf2bb937ca640da5243171f65a35ed6901b
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-04-28 09:09:29 -0300

    Anotei as coisas aprendidas na aula

---
+++ aula6.py
@@ -0,0 +1,11 @@
+compras = ['chipanzini bananini', 'tralalelo tralala', 'bombardiro crocodilo']
+for i in compras:
+    print(i)
+print("")
+arrayCompras = ["Banana", 10, 15.25]
+for x in range(len(arrayCompras)):
+    print(arrayCompras[x])
+
+compras.append(10)
+compras.insert(1,"Bailarina Cappuchina")
+print(compras)
