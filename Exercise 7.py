1979d2f57a32af0fb8c09bc9f25c8b12420682e1
Kleber Jr. <kleberwagner107@gmail.com>
2025-04-30 22:28:21 -0300

    Criei um codigo que lê 5 usuarios e senhas, e depois exibe os usuarios e suas respectivas senhas

---
+++ Exercise 7.py
@@ -0,0 +1,9 @@
+usuario = ["", "", "", "", ""]
+senha = ["", "", "", "", ""]
+
+for i in range(len(usuario)):
+    usuario[i] = input("Crie seu nome de usuário:\n")
+    senha[i] = input("Crie uma senha para o seu usuário usuario:\n")
+
+for i in range(len(usuario)):
+    print(f"{i+1}. usuário: {usuario[i]}, senha: {senha[i]}")
